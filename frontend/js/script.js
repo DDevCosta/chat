b10b71ce4616da8ffdb30d86e702f5ad65b14072
DDevCosta <DDevCosta@gmail.com>
2024-04-02 21:45:02 -0300

    Função random user colors

---
+++ frontend/js/script.js
@@ -0,0 +1,34 @@
+const login = document.querySelector(".login")
+const loginForm = document.querySelector(".login__form")
+const loginInput = document.querySelector(".login__input")
+
+const colors = [
+    "cadetblue",
+    "darkgoldenrod",
+    "cornflowerblue",
+    "darkkhaki",
+    "hotpink",
+    "gold"
+]
+
+const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length)
+    return colors[randomIndex]
+}
+
+const user = {
+    id: "",
+    name: "",
+    color: ""
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault()
+
+    user.id = crypto.randomUUID()
+    user.name = loginInput.value
+
+    console.log(Math.random())
+}
+
+loginForm.addEventListener("submit", handleSubmit)
